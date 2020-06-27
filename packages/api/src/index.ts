1847d7cab6a01b1f45a49a835f7df94f547722d2
bowen <xboxue@gmail.com>
2020-06-27 14:54:40 -0400

    Update API config for deploying

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,12 @@ import * as admin from "firebase-admin";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { Container } from "typedi";
-import { createConnection, getRepository, useContainer } from "typeorm";
+import {
+  createConnection,
+  getConnectionOptions,
+  getRepository,
+  useContainer
+} from "typeorm";
 import { User } from "./entities/User";
 
 const main = async () => {
@@ -14,36 +19,46 @@ const main = async () => {
   });
 
   useContainer(Container);
-  await createConnection();
 
-  const schema = await buildSchema({
-    resolvers: [__dirname + "/resolvers/**/*.ts"],
-    container: Container
-  });
-
-  const server = new ApolloServer({
-    schema,
-    context: async ({ req }) => {
-      try {
-        const token = req.headers.authorization.split(" ")[1];
-        const { uid } = await admin.auth().verifyIdToken(token);
-
-        const userRepository = getRepository(User);
-        const user = await userRepository.findOne(uid);
-
-        return { user };
-      } catch (error) {
-        throw new AuthenticationError("Invalid user");
-      }
-    }
-  });
-
-  const app = express();
-  server.applyMiddleware({ app });
-
-  app.listen({ port: 4000 }, () =>
-    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+  const options = await getConnectionOptions(
+    process.env.NODE_ENV || "development"
   );
+  await createConnection({ ...options, name: "default" });
+
+  try {
+    const schema = await buildSchema({
+      resolvers: [__dirname + "/resolvers/**/*.{js,ts}"],
+      container: Container
+    });
+
+    const server = new ApolloServer({
+      schema,
+      context: async ({ req }) => {
+        try {
+          const token = req.headers.authorization.split(" ")[1];
+          const { uid } = await admin.auth().verifyIdToken(token);
+
+          const userRepository = getRepository(User);
+          const user = await userRepository.findOne(uid);
+
+          return { user };
+        } catch (error) {
+          throw new AuthenticationError("Invalid user");
+        }
+      }
+    });
+
+    const app = express();
+    server.applyMiddleware({ app });
+
+    const port = process.env.PORT || 4000;
+    app.listen({ port }, () =>
+      console.log(`Server ready at http://localhost:${port}/graphql`)
+    );
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 main();
